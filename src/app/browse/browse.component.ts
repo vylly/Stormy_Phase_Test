aca714e216abc4b30b8aef6a0abcda73107ecaef
Vincent RENOTTE <vincent.renotte@gmail.com>
2018-11-12 16:16:55 +0100

    integrates scanner in new tab view from commit 4b7cda

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 
-// ITEM TAB
+//import { registerElement } from "nativescript-angular/element-registry";
+// registerElement("MLKitBarcodeScanner", () => require("nativescript-plugin-firebase/mlkit/barcodescanning").MLKitBarcodeScanner);
+
+import { MLKitScanBarcodesOnDeviceResult } from "nativescript-plugin-firebase/mlkit/barcodescanning";
 
 
 @Component({
@@ -9,11 +12,37 @@ import { Component, OnInit } from "@angular/core";
     templateUrl: "./browse.component.html"
 })
 export class BrowseComponent implements OnInit {
+    barcodes: Array<{
+        value: string;
+        format: string;
+      }>;
+
+      pause: boolean = false;
+
     constructor() {
-        // Use the component constructor to inject providers.
+        /* ***********************************************************
+        * Use the constructor to inject app services that you need in this component.
+        *************************************************************/
     }
 
     ngOnInit(): void {
-        // Use the "ngOnInit" handler to initialize data for the view.
+        /* ***********************************************************
+        * Use the "ngOnInit" handler to initialize data for this component.
+        *************************************************************/
+    }
+
+    //Function to call every X sec
+    public onScan(event: any): void {
+        
+        const result: MLKitScanBarcodesOnDeviceResult = event.value;
+        this.barcodes = result.barcodes;
+        console.log("resultat this.barcodes: " + JSON.stringify(this.barcodes));
+
+        if (this.barcodes.length > 0) {
+            console.log("pausing the scanner for 3 seconds (to test the 'pause' feature)");
+            this.pause = true;
+            alert("code scannééé !");
+            setTimeout(() => this.pause = false, 3000)
+          }
     }
 }
