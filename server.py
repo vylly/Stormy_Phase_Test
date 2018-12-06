d4db3a4fa74ec7403b0fca178c8d2e27c6b86423
max marie <max.marie.4@gmail.com>
2018-12-06 14:44:42 +0100

    server commented

--- server.py
+++ server.py
@@ -4,15 +4,33 @@ import json
 
 app = Flask(__name__)
 
-listItems = []
+json_data=open('data.json').read()
+listItems = json.loads(json_data)
+# ============================= ROUTES ============================
 
+# Route / : get all the list of items
 @app.route('/', methods=['GET'])
 def all():
-    global listItems
-    json_data=open('data.json').read()
-    listItems = json.loads(json_data)
     return jsonify(listItems)
 
+# Route /getfromid/<idItem> : return an item using its id
+@app.route('/getfromid/<idItem>')
+def getfromid(idItem):
+    global listItems
+    selectedItems = [item for item in listItems if item["id"] == int(idItem)] # returns list of the items with same id (should have length 0 or 1)
+    if len(selectedItems) == 0:
+        abort(404)
+    else:
+        return jsonify(selectedItems[0])
+
+# Route /gefromname/<nameItem> : return a list of item using the name
+@app.route('/getfromname/<nameItem>')
+def getfromname(nameItem):
+    global listItems
+    selectedItems = [item for item in listItems if item["name"] == nameItem] # returns list of the items with same id so we take the first cell
+    return jsonify(selectedItems)
+
+# Route /add : method post, add a new item to the database
 @app.route('/add', methods=['POST'])
 def add():
     if not request.json or not 'name' in request.json:
@@ -22,10 +40,16 @@ def add():
         'name': request.json['name'],
         'parent': request.json['parent']
     }
+    # add the item to the list
     listItems.append(item)
+    # write the new list in the database
     with open('data.json', 'w') as outfile:
         json.dump(listItems, outfile)
+    # return the new item, with the id just generated
     return jsonify({'newItem': item}), 201
+# ============================= ROUTES ============================
 
+
+# --------------- run server ---------------
 if __name__ == '__main__':
     app.run(debug=True, port=80)
