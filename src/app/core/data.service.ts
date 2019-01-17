1bbfbe1815ea813119fefd758dc6157bc1eb058e
max marie <max.marie.4@gmail.com>
2019-01-17 20:10:51 +0100

    ip changed

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -32,8 +32,8 @@ export class User {
 @Injectable()
 export class DataService {
 
-    //protected IP_Server: String = "51.38.68.118:5000"
-    protected IP_Server: String = "192.168.1.14:5000"
+    protected IP_Server: String = "51.38.68.118:5000"
+    //protected IP_Server: String = "192.168.1.14:5000"
 
     protected members = new Array<IMember>();
 
