aa5e19f713ea8edaf2032cd21eb3151810afc551
max marie <max.marie.4@gmail.com>
2019-01-21 23:24:56 +0100

    check if email is an email and pwds are the same on register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,7 +5,8 @@ import { Page, isUserInteractionEnabledProperty } from "tns-core-modules/ui/page
 import { DataService, User } from "../core/data.service";
 import { TextField } from "tns-core-modules/ui/text-field";
 import { request, getJSON, HttpRequestOptions } from "tns-core-modules/http";
-import {ISpace } from "../core/data.service"; 
+import { ISpace } from "../core/data.service";
+import * as EmailValidator from 'email-validator';
 //import * as Https from 'nativescript-https'
 
 @Component({
@@ -51,22 +52,22 @@ export class LoginComponent {
 
     // Display a prompt to enter the email address and find the password
     forgotPassword() {
-    prompt({
-        title: "Forgot Password",
-        message: "Enter the email address you used to register.",
-        defaultText: "",
-        okButtonText: "Ok",
-        cancelButtonText: "Cancel"
-    }).then((data) => {
-        if (data.result) {
-        // Call the backend to reset the password
-        alert({
-            title: "APP NAME",
-            message: "Sorry, this has not been implemented yet :(",
-            okButtonText: "Ok"
-        })
-        }
-    });
+        prompt({
+            title: "Forgot Password",
+            message: "Enter the email address you used to register.",
+            defaultText: "",
+            okButtonText: "Ok",
+            cancelButtonText: "Cancel"
+        }).then((data) => {
+            if (data.result) {
+                // Call the backend to reset the password
+                alert({
+                    title: "APP NAME",
+                    message: "Sorry, this has not been implemented yet :(",
+                    okButtonText: "Ok"
+                })
+            }
+        });
     }
 
     // Test the password and email and log in the user
@@ -89,49 +90,61 @@ export class LoginComponent {
             let id = response.content.toJSON().id;
             let name = response.content.toJSON().name;
             let space = response.content.toJSON().space;
-            if(id == -1) {
+            if (id == -1) {
                 alert("Wrong email or password. Please retry.");
             } else {
                 this.user.id = id;
                 this.user.name = name;
                 let listSpaces = response.content.toJSON().spaces;
-                listSpaces.forEach(sp => this.user.spaces.push({id: sp.id, name: sp.name})); // need to get the name here from the back end
+                listSpaces.forEach(sp => this.user.spaces.push({ id: sp.id, name: sp.name })); // need to get the name here from the back end
                 this.dataService.setCurrentUser(this.user);
-                this.routerExtension.navigate(["../spaces"], {clearHistory: true});
+                this.routerExtension.navigate(["../spaces"], { clearHistory: true });
             }
-        }, (e) => {});
+        }, (e) => { });
     }
 
     // Create the user in the backend and go to login again
     register() {
-        request({
-            url: "http://" + this.dataService.getIPServer() + "/signup",
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            content: JSON.stringify({
-                email: this.user.email,
-                password: this.user.password,
-                name: this.user.name
-            })
-            // body: {
-            //     "content" : JSON.stringify({
-            //         email: this.user.email,
-            //         password: this.user.password,
-            //         name: this.user.name
-            //     })
-            // }
-        }).then((response) => {
-            let id = response.content.toJSON().id;
-            if(id == -1) {
-                alert("Email already in use.");
+        // Check if the email is actually a valid email address
+        if (EmailValidator.validate(this.user.email)) {
+            if(this.user.confirmPassword == this.user.password) {
+                request({
+                    url: "http://" + this.dataService.getIPServer() + "/signup",
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    content: JSON.stringify({
+                        email: this.user.email,
+                        password: this.user.password,
+                        name: this.user.name
+                    })
+                    // body: {
+                    //     "content" : JSON.stringify({
+                    //         email: this.user.email,
+                    //         password: this.user.password,
+                    //         name: this.user.name
+                    //     })
+                    // }
+                }).then((response) => {
+                    let id = response.content.toJSON().id;
+                    if (id == -1) {
+                        alert("Email already in use.");
+                    } else {
+                        let listSpaces = response.content.toJSON().spaces;
+                        listSpaces.forEach(sp => this.user.spaces.push({ id: sp.id, name: sp.name })); // need to get the name here from the back end
+                        this.user.id = id;
+                        this.dataService.setCurrentUser(this.user);
+                        this.routerExtension.navigate(["../spaces"], { clearHistory: true });
+                    }
+                }, (e) => { });
             } else {
-                let listSpaces = response.content.toJSON().spaces;
-                listSpaces.forEach(sp => this.user.spaces.push({id: sp.id, name: sp.name})); // need to get the name here from the back end
-                this.user.id = id;
-                this.dataService.setCurrentUser(this.user);
-                this.routerExtension.navigate(["../spaces"], {clearHistory: true});
+                alert("The two passwords are not the same");
+                return;
             }
-        }, (e) => {});
+            
+        } else {
+            alert("Please enter a valid email address.");
+            return;
+        }
     }
 
     focusPassword() {
@@ -154,8 +167,8 @@ export class LoginComponent {
     }
     onChangePwd(args) {
         let textField = <TextField>args.object;
-        this.user.password =textField.text;
-        
+        this.user.password = textField.text;
+
     }
     onChangeConfPwd(args) {
         let textField = <TextField>args.object;
